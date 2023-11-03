1ca605a53033d7ac17b27eb1302c62504281b772
Peter8790 <you@example.com>
2023-11-03 13:44:22 +0100

    Endpoint functions

--- flask-mvc-example/project/models/my_dao.py
+++ flask-mvc-example/project/models/my_dao.py
@@ -1,8 +1,8 @@
 from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
 import json
 
-URI = "neo4j+ssc://2717129e.databases.neo4j.io"
-AUTH = ("neo4j", "4zFuuuJy9DxyarHmxaBlBkGr8T7U2At48icm3GbB_Do")
+URI = "neo4j+s://2432849c.databases.neo4j.io"
+AUTH = ("X0VQBfXWOkSDhuKG2DS8CIbnSQclMX7q2K2a1d4_H_Q")
 
 def _get_connection() -> Driver:
     driver = GraphDatabase.driver(URI, auth=AUTH)
@@ -45,3 +45,38 @@ def update_car(make, model, reg, year, capacity):
 
 def delete_car(reg):
     _get_connection().execute_query("MATCH (a:Car{reg: $reg}) delete a;", reg = reg)
+
+
+#ENDDDDDDDDDDDDDDDDDDDPOINTS
+#Implement an endpoint ‘cancel-order-car’ where a customer-id, car-id is passed as parameters. 
+
+def cancel_order_car(customer_id, car_id):
+    with _get_connection().session() as session:
+        cars = session.run("MATCH (a:Customer{customer_id:$customer_id})-[r:BOOKED]->(b:Car{car_id:$car_id}) DELETE r;", customer_id=customer_id, car_id=car_id)
+        print(cars)
+        nodes_json = [node_to_json(record["a"]) for record in cars]
+        print(nodes_json)
+        return nodes_json
+
+
+
+
+#Implement an endpoint ‘rent-car’ where a customer-id, car-id is passed as parameters.
+
+
+def rent_car(customer_id, car_id):
+    with _get_connection().session() as session:
+        cars = session.run("MATCH (a:Customer{customer_id:$customer_id})-[r:BOOKED]->(b:Car{car_id:$car_id}) set b.status = 'rented' RETURN b;", customer_id=customer_id, car_id=car_id)
+        print(cars)
+        nodes_json = [node_to_json(record["b"]) for record in cars]
+        print(nodes_json)
+        return nodes_json
+    
+#Implement an endpoint ‘return-car’ where a customer-id, car-id is passed as parameters.
+def return_car(customer_id,car_id):
+    with _get_connection().session() as session:
+        cars = session.run("MATCH (a:Customer{customer_id:$customer_id})-[r:BOOKED]->(b:Car{car_id:$car_id}) set b.status = 'available' RETURN b;", customer_id=customer_id, car_id=car_id)
+        print(cars)
+        nodes_json = [node_to_json(record["b"]) for record in cars]
+        print(nodes_json)
+        return nodes_json
